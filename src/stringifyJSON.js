42bacfea77b8307ca20d3d73dbf2bff4501b2410
Yara TP <ytercero36@gmail.com>
2016-07-10 04:02:20 -0400

    Definitely getting closer. Had a lightbulb moment and feel more confident with the direction I'm going now.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,39 +4,72 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  if(typeof obj === "string" || typeof obj === "boolean" || typeof obj === "number"){
-    return "'" + obj + "'";
-  }
+	var result;
+	var recurse = false;
 
-  var final;
+	if(typeof obj !== "object" || obj === null){
+    	return "'" + obj + "'";
+  	}
 
-  if(typeof obj === "object"){
-    var result = reduce(obj, function(startVal, item, key){
-      var str = "'" + key + "'";
-      var startVal;
-      var recurse = false;
-      if(typeof item === "object"){
-        recurse = true;
-        startVal[key] = stringifyJSON(item);
-        console.log("1:" + JSON.stringify(startVal));
-        console.log("2:" + JSON.stringify(startVal[key]));
-        recurse = false;
-        return startVal;
-      } else if(typeof item === "string"){
-        item = "" + item + "";
-      }
+  	if(typeof obj === "string"){
+  		return "'" + obj + "'";
+  	}
 
-      startVal[str] = item;
-      console.log("3:" + JSON.stringify(startVal));
-      //console.log(startVal);
-      if(recurse === false){
-      	return startVal;
-      }
+  	if(typeof obj !== "string" && typeof obj !== "object" && recurse === true){
+  		return obj;
+  	}
+
+  	if(Array.isArray(obj)){
+  		result = [];
+
+  		_.each(obj, function(item){
+  			recurse = true;
+  			result.push(stringifyJSON(item));
+  			recurse = false;
+  		})
+
+  		return result;
+  	}
+
+  	if(typeof obj === "object" && !Array.isArray(obj)){
+  		result = {};
+
+  		_.each(obj, function(item, key){
+  			recurse = true;
+  			result[key] = stringifyJSON(item);
+  			recurse = false;
+  		});
+
+  		return result;
+  	}
+
+ //  if(typeof obj === "object"){
+ //    var result = reduce(obj, function(startVal, item, key){
+ //      var str = "'" + key + "'";
+ //      var startVal;
+ //      var recurse = false;
+ //      if(typeof item === "object"){
+ //        recurse = true;
+ //        startVal[key] = stringifyJSON(item);
+ //        console.log("1:" + JSON.stringify(startVal));
+ //        console.log("2:" + JSON.stringify(startVal[key]));
+ //        recurse = false;
+ //        return startVal;
+ //      } else if(typeof item === "string"){
+ //        item = "" + item + "";
+ //      }
+
+ //      startVal[str] = item;
+ //      console.log("3:" + JSON.stringify(startVal));
+ //      //console.log(startVal);
+ //      if(recurse === false){
+ //      	return startVal;
+ //      }
       
-    }, {})
-	console.log(result);
-  	return result;
-  }
+ //    }, {})
+	// console.log(result);
+ //  	return result;
+ //  }
   
 
 
