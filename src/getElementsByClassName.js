d973cf63fbd489827cb912c94c5d4e0affd11b65
Yara TP <ytercero36@gmail.com>
2016-07-10 15:10:29 -0400

    Starting super simple, just determining if body has child nodes.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,17 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+ var element = document.body;
+
+  var children = element.childNodes;
+  console.log(children);
+  var result = [];
+
+  if(element.hasChildNodes()){
+    return console.log("has child nodes");
+  } else{
+    return console.log("does not have child nodes");
+  }
 };
