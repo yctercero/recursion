4e6f52ad95fa5e6aefff865e535dbd5c5fb7f5e0
Yara TP <ytercero36@gmail.com>
2016-07-10 13:43:52 -0400

    Completed! I'd had the conditional statements and return statements in the wrong places, once I began seperating out the conditional statements, the recursive aspect of the function began to make sense. Note to self: don't forget the basics-break down the problem!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,74 +5,56 @@
 
 var stringifyJSON = function(obj) {
 	var result;
-	var recurse = false;
+	var isFirst = true;
 
-	if(typeof obj !== "object" || obj === null){
-    	return "'" + obj + "'";
+	if(typeof obj === "undefined" || typeof obj === "function"){
+		//console.log("1");
+		return "";
+	}
+
+	if(obj === null){
+		//console.log("2");
+    	return "" + obj + "";
   	}
 
   	if(typeof obj === "string"){
-  		return "'" + obj + "'";
+  		//console.log("3");
+  		return "\"" + obj + "\"";
   	}
 
-  	if(typeof obj !== "string" && typeof obj !== "object" && recurse === true){
-  		return obj;
+  	if(typeof obj !== "object"){
+  		//console.log("4");
+  		return "" + obj + "";
   	}
 
   	if(Array.isArray(obj)){
   		result = [];
-
+  		//console.log("5");
   		_.each(obj, function(item){
-  			recurse = true;
   			result.push(stringifyJSON(item));
-  			recurse = false;
   		})
 
-  		return result;
+  		return "[" + result + "]";
   	}
 
-  	if(typeof obj === "object" && !Array.isArray(obj)){
-  		result = {};
-
+  	if(typeof obj === "object"){
+  		result = "";
+  		var temp = [];
+  		//console.log("6");
   		_.each(obj, function(item, key){
-  			recurse = true;
-  			result[key] = stringifyJSON(item);
-  			recurse = false;
+  			//key = "\'" + key + "\'";
+				var log = stringifyJSON(item);
+				if(log === ""){
+					result = "";
+				} else{
+					temp.push("\"" + key + "\"" + ":" + log);
+					result = temp.join(',');
+				}
+  			
+  			
   		});
 
-  		return result;
+  		return "{" + result + "}";
   	}
-
- //  if(typeof obj === "object"){
- //    var result = reduce(obj, function(startVal, item, key){
- //      var str = "'" + key + "'";
- //      var startVal;
- //      var recurse = false;
- //      if(typeof item === "object"){
- //        recurse = true;
- //        startVal[key] = stringifyJSON(item);
- //        console.log("1:" + JSON.stringify(startVal));
- //        console.log("2:" + JSON.stringify(startVal[key]));
- //        recurse = false;
- //        return startVal;
- //      } else if(typeof item === "string"){
- //        item = "" + item + "";
- //      }
-
- //      startVal[str] = item;
- //      console.log("3:" + JSON.stringify(startVal));
- //      //console.log(startVal);
- //      if(recurse === false){
- //      	return startVal;
- //      }
-      
- //    }, {})
-	// console.log(result);
- //  	return result;
- //  }
-  
-
-
-
-  
+ 
 };
