860b269e0497fdd805505f84165dbc7313e860dc
Yara TP <ytercero36@gmail.com>
2016-07-10 02:14:59 -0400

    Semi-functional. Understand that the recursive part of this really comes in with nested elements -- arrays/objects -- and trying to work on one at a time. Currently working on having it function with objects. Piece by piece!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(typeof obj === "string" || typeof obj === "boolean" || typeof obj === "number"){
+    result = "'" + obj + "'";
+  }
+
+  // var dict = {
+  //   true: "'true'",
+  //   false: "'false'",
+  //   number: 
+  // }
+
+  var final;
+
+  if(typeof obj === "object"){
+    var result = reduce(obj, function(startVal, item, key){
+      var str = "'" + key + "'";
+      var startVal;
+      if(typeof item === "object"){
+        //startVal = startVal[key];
+        startVal[key] = stringifyJSON(item);
+        console.log("1:" + JSON.stringify(startVal));
+        console.log("2:" + JSON.stringify(startVal[key]));
+        //startVal[key] = 
+      } else if(typeof item === "string"){
+        item = "" + item + "";
+      }
+
+      startVal[str] = item;
+      console.log("3:" + JSON.stringify(startVal));
+      //console.log(startVal);
+      return startVal;
+    }, {})
+  }
+  
+
+
+
+  console.log(result);
+  return result;
 };
