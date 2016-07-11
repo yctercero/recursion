acd89d7187af56a7fe34656aa52b238085fa65b5
Yara TP <ytercero36@gmail.com>
2016-07-11 14:04:48 -0400

    Created multiple separate functions, one to start the traversal of the string, and separate functions for objects, arrays, and strings. So far only written somethgin for arrays. Taking it piece by piece.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,38 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  // Super helpful - http://ronfenolio.com/blog/2016/01/parsing-json/
+
+  var results;
+  var keyAt; //keep track of where in the string we are
+  var stringLength = json.length;
+
+  each(json, function(item, key, collection){
+
+  	if(item === "["){
+  		keyAt = key;
+  		parseArray(item, keyAt);
+  	}
+
+  });
+
+  //Object function
+
+  // Array function
+  function parseArray(item, keyAt){
+  	for (var i = keyAt; i <= stringLength; i++) {
+  		
+  		console.log(json[i]);
+
+  		if(json[i] === "]"){
+  			results = [];
+  		}
+  		
+  	}
+  }
+
+  // String function
+
+
+  return results;
 };
